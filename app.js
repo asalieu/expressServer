2d98284b2e58cd487e52370a7ef915b2f80003b3
salieu <sowealieu@gmail.com>
2019-10-11 14:47:53 +0200

    okay

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var PORT = process.env.PORT || 5000;
 //var datats = import './fetch_data.js';
 //var datas = require('./fetch_data.js')
 const app = express()
@@ -13,4 +14,4 @@ app.get('/measurement',(req,res)=>{
      
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 5000, () => console.log(`Example app listening on port ${port}!`))
